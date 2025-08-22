4cb23d75708e3d2a8db8bc7443806052596a161c
DaNiLo <danilo.p2391@gmail.com>
2025-08-22 14:03:46 +0200

    Define helper function to get users + prettier CLI messages

--- filter_users.py
+++ filter_users.py
@@ -1,21 +1,30 @@
 import json
 
 
+def get_users():
+    """ Gets and returns users from users.json """
+    with open("users.json", "r", encoding="utf-8") as file:
+        users = json.load(file)
+
+    return users
+
+
 def filter_users_by_name(name):
     """
     Gets a string to filter and print all the matching users by 'name'
     from users.json
     """
-    with open("users.json", "r") as file:
-        users = json.load(file)
+    users = get_users()
 
-    filtered_users = [user for user in users if user["name"].lower() == name.lower()]
+    filtered_users = [
+        user for user in users if user["name"].lower() == name.lower()
+    ]
 
     if len(filtered_users) == 0:
-        print("No user found")
+        print("❌ No user found")
 
     for user in filtered_users:
-        print(user)
+        print("😄", user)
 
 
 def filter_users_by_age(age):
@@ -23,16 +32,15 @@ def filter_users_by_age(age):
     Gets an integer to filter and print all the matching users by 'age'
     from users.json
     """
-    with open("users.json", "r") as file:
-        users = json.load(file)
+    users = get_users()
 
     if age.isdigit():
         filtered_users = [user for user in users if user["age"] == int(age)]
 
         for user in filtered_users:
-            print(user)
+            print("😄", user)
     else:
-        print("'Age' must be an integer.")
+        print("❌ 'Age' must be an integer.")
 
 
 def filter_users_by_email(email):
@@ -40,16 +48,15 @@ def filter_users_by_email(email):
     Gets an email to filter and print all the matching users by 'email'
     from users.json
     """
-    with open("users.json", "r") as file:
-        users = json.load(file)
+    users = get_users()
 
     if '@' in email:
         filtered_users = [user for user in users if user["email"] == email]
 
         for user in filtered_users:
-            print(user)
+            print("😄", user)
     else:
-        print("'email' is not valid.")
+        print("❌ 'email' is not valid.")
 
 
 def main():
@@ -61,13 +68,14 @@ def main():
     }
 
     filter_option = input(
-        "What would you like to filter by? (Only 'name', 'age' and 'email' are supported): ").strip().lower()
+        "What would you like to filter by? "
+        "(Only 'name', 'age' and 'email' are supported): ").strip().lower()
 
     if filter_option in filter_dispatcher:
         search_value = input(f"Enter the {filter_option} to filter users: ").strip()
         filter_dispatcher[filter_option](search_value)
     else:
-        print("Filtering by that option is not yet supported.")
+        print("❌ Filtering by that option is not supported.")
 
 
 if __name__ == "__main__":
